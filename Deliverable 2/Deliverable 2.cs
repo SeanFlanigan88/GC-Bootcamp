542f1be5edf99c2647000bd4bd285d831f31903d
Sean Flanigan <seanflaniganjr6588@gmail.com>
2021-07-22 16:05:30 -0400

    Deliverable 2 w/o loop V2

---
+++ Deliverable 2/Deliverable 2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Deliverable_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What would you like to say to the Chat Bot ?");
+            string response = Console.ReadLine().ToLower();
+
+            string answer = null;
+            do
+            {
+                switch (response)
+                {
+                    case "hello":
+                        Console.WriteLine("Hi, good to see you !" + "\nWould you like to continue y/n ?");
+                        string response1 = Console.ReadLine();
+                        if (response1 == "hello")
+                        { 
+                            Console.WriteLine("I'm sorry, but you have already said that."); 
+                        }
+                        break;
+
+                    case "sup":
+                        Console.WriteLine("I am good" + "\nWould you like to continue y/n ?");
+                        string response2 = Console.ReadLine();
+                        if (response2 == "sup")
+                        {
+                            Console.WriteLine("I'm sorry, but you have already said that.");
+                        }
+                        break;
+
+                    case "hello there":
+                        Console.WriteLine("General Kenobi" + "\nWould you like to continue y/n ?");
+                        string response3 = Console.ReadLine();
+                        if (response3 == "hello there")
+                        {
+                            Console.WriteLine("I'm sorry, but you have already said that.");
+                        }
+                        break;
+
+                    case "bye":
+                        Console.WriteLine("Good Bye !");
+                        break;
+                    default:
+                        break;
+
+                }
+            } while (response == "y");
+        }
+    }
+}
