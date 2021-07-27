0eb9a47ac99458f4201fd4452648e2f6a014c513
Sean Flanigan <seanflaniganjr6588@gmail.com>
2021-07-27 15:11:00 -0400

    Deliverable 1 V2 Final

--- Deliverable 1/Deliverable 1/Program.cs
+++ Deliverable 1/Deliverable 1/Program.cs
@@ -21,34 +21,34 @@ namespace Deliverable_1
             { switch (input)
                 {
                     case "a":
-                        Console.WriteLine("Please enter a number ");
+                        Console.WriteLine("Please enter a number in Inches");
                         string input1 = Console.ReadLine().ToLower();
-                        int num1 = int.Parse(input1);
+                        decimal num1 = decimal.Parse(input1);
                         int num11 = imperialInch;
                         decimal num111 = milSpin;
-                        Console.WriteLine((num1 * num11) * num111);
+                        Console.WriteLine((num1 * num11) * num111 + " Fidget Spinners");
                         break;
                     case "b":
-                        Console.WriteLine("Please enter a number ");
+                        Console.WriteLine("Please enter a number in Fidget Spinners");
                         string input2 = Console.ReadLine().ToLower();
-                        int num2 = int.Parse(input2);
+                        decimal num2 = decimal.Parse(input2);
                         decimal num22 = milSpin;
-                        Console.WriteLine(num22 * num2);
+                        Console.WriteLine(num22 * num2 + " Inches");
                         break;
                     case "c":
-                        Console.WriteLine("Please enter a number ");
+                        Console.WriteLine("Please enter a number in Feet");
                         string input3 = Console.ReadLine().ToLower();
-                        int num3 = int.Parse(input3);
+                        decimal num3 = decimal.Parse(input3);
                         int num33 = imperialFoot;
                         int num333 = milMeme;
-                        Console.WriteLine((num3 * num33) * num333);
+                        Console.WriteLine((num3 * num33) * num333 + " Memes");
                         break;
                     case "d":
-                        Console.WriteLine("Please enter a number");
+                        Console.WriteLine("Please enter a number in Memes");
                         string input4 = Console.ReadLine().ToLower();
-                        int num4 = int.Parse(input4);
+                        decimal num4 = decimal.Parse(input4);
                         int num44 = milMeme;
-                        Console.WriteLine(num44 * num4);
+                        Console.WriteLine(num44 * num4 + " Feet");
                         break;
                 }
                 Console.WriteLine("Would you like to enter another # y/n ?");
